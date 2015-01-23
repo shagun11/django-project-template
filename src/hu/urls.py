0b1407ce61c2762ac9c2eba6cb101ba9030bbf5b
Anchit Gupta <anchit.gupta@hashedin.com>
2015-01-23 17:26:01 +0530

    usecase 04 done

--- src/hu/urls.py
+++ src/hu/urls.py
@@ -8,7 +8,8 @@ urlpatterns = patterns('',
     url(r'^$', 'hu.views.home', name='home'),
     url(r'^tasks/$','hu.views.get_tasks',name='get_tasks'),
     url(r'^tasks/pending$', 'hu.views.tasks_pending'),
-    url(r'^details/', 'hu.views.get_details',name='movie_detail')
+    url(r'^details/', 'hu.views.get_details',name='movie_detail'),
+    url(r'^search/', 'hu.views.search',name='search')
     
 )
 
--- src/hu/views.py
+++ src/hu/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 import datetime
+from django.db.models import Q
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 import models
@@ -11,10 +12,10 @@ def home(request):
        # print ">...........", request.GET 
         
         count = 0
-        #print "nsme.....", name
+        # print "nsme.....", name
         movie_object = models.Movie.objects.get(movie_name=name)
         
-        #print "dsfsd",movie_object
+        # print "dsfsd",movie_object
         rate = models.Rating(movie=movie_object, sum_rating=int(rating), rating_count=count)
         rate.save()
         # print("Movie name is: " + movie_name)
@@ -23,7 +24,7 @@ def home(request):
         
         t = models.Movie.objects.order_by('-date')
         t1 = models.Movie.objects.order_by('-added_at_time')
-    return render(request, 'home.html', {"tasks":t, "tasks1":t1,})
+    return render(request, 'home.html', {"tasks":t, "tasks1":t1, })
 
 def alltasks(request):
     
@@ -39,4 +40,14 @@ def get_details(request):
     get_movie = request.GET.get('mov_name')
     movie_detail = models.Movie.objects.filter(movie_name=get_movie)
     genre_all = models.Genre.objects.filter()
-    return render(request,'movieDetail.html',{"movie_detail":movie_detail,"genre_all":genre_all})
+    return render(request, 'movieDetail.html', {"movie_detail":movie_detail, "genre_all":genre_all})
+
+def search(request):
+    get_movie_name = request.POST.get('search_name')
+    get_movie_date = request.POST.get('year')
+    print "vjhfjgkjkhvkglhl", get_movie_name
+    results = models.Movie.objects.filter(Q(movie_name__icontains=get_movie_name),Q(date__icontains=get_movie_date))
+    # print movie_search
+    genre_all = models.Genre.objects.filter()
+    return render(request, 'searchMovie.html', {"movie_search":results, "genre_all":genre_all})
+
